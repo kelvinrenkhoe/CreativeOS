8418769dd0418b27004573b846f16667aaab0d0a
kelvin <37311143+kelvinrenkhoe@users.noreply.github.com>
2026-08-03 21:27:50 +0100

    Add extensible doctor check framework (#113)
    * feat(doctor): add health-check package
    
    * feat(doctor): add extensible check registry
    
    * feat(doctor): execute registered extension checks

---
+++ doctor/__init__.py
@@ -0,0 +1,5 @@
+"""Extensible health-check framework for CreativeOS Doctor."""
+
+from doctor.framework import DoctorCheckProvider, DoctorCheckRegistry
+
+__all__ = ["DoctorCheckProvider", "DoctorCheckRegistry"]
