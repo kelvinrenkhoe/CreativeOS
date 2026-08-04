ba402a89e8bfcf093e65995456be51ca9e004868
kelvin <37311143+kelvinrenkhoe@users.noreply.github.com>
2026-08-04 18:50:34 +0100

    feat: register campaign dashboard command

--- cli/__init__.py
+++ cli/__init__.py
@@ -1,5 +1,6 @@
 """CreativeOS CLI package registrations."""
 
+from cli import campaign_dashboard as _campaign_dashboard
 from cli import campaign_timeline as _campaign_timeline
 
-__all__ = ("_campaign_timeline",)
+__all__ = ("_campaign_dashboard", "_campaign_timeline")
