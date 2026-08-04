d11c599b0d1fe574a0807907b4df75a5bbb497bb
kelvin <37311143+kelvinrenkhoe@users.noreply.github.com>
2026-08-04 21:07:03 +0100

    feat: register campaign today command

--- cli/__init__.py
+++ cli/__init__.py
@@ -1,6 +1,7 @@
 """CreativeOS CLI package registrations."""
 
 from cli import campaign_dashboard as _campaign_dashboard
+from cli import campaign_manager as _campaign_manager
 from cli import campaign_timeline as _campaign_timeline
 
-__all__ = ("_campaign_dashboard", "_campaign_timeline")
+__all__ = ("_campaign_dashboard", "_campaign_manager", "_campaign_timeline")
