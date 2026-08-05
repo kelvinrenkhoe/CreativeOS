0ed390d9aade284d776760f04633cc25ad112901
kelvin <37311143+kelvinrenkhoe@users.noreply.github.com>
2026-08-05 08:53:11 +0100

    feat: register AI campaign plan command

--- cli/main.py
+++ cli/main.py
@@ -14,6 +14,7 @@ from cli.campaign_fix import fix_command
 from cli.campaign_fix_rollback import rollback_plan_command
 from cli.campaign_fix_rollback_execute import rollback_command
 from cli.campaign_history import campaign_history_command
+from cli.campaign_plan import campaign_plan_command
 from cli.campaign_run import campaign_run_command
 from cli.index import app as index_app
 from cli.repository import search_command, stats_command
@@ -45,6 +46,7 @@ campaign_app.registered_commands = [
     command for command in campaign_app.registered_commands if command.name != "run"
 ]
 campaign_app.command("run")(campaign_run_command)
+campaign_app.command("plan")(campaign_plan_command)
 campaign_app.command("history")(campaign_history_command)
 campaign_app.add_typer(campaign_checkpoint_app, name="checkpoint")
 campaign_app.add_typer(week_plan_app, name="week")
